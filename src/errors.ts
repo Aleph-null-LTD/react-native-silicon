ac978456e54dbc5e387950a33d6113650fbf3a9a
SSHardy <sammy@aleph-null.co.uk>
2026-05-10 03:10:23 +0100

    refactor: Add errors

---
+++ src/errors.ts
@@ -0,0 +1,27 @@
+export enum SiliconErrorCode {
+  PLATFORM_NOT_SUPPORTED = 'PLATFORM_NOT_SUPPORTED',
+  KEY_NOT_FOUND ='KEY_NOT_FOUND',
+  HARDWARE_NOT_AVAILABLE = 'HARDWARE_NOT_AVAILABLE',
+  BIOMETRICS_NOT_ENROLLED = 'BIOMETRICS_NOT_ENROLLED',
+  USER_CANCELLED = 'USER_CANCELLED',
+  INVALID_SIGNATURE = 'INVALID_SIGNATURE',
+  ATTESTATION_FAILED = 'ATTESTATION_FAILED',
+  UNKNOWN_NATIVE_ERROR = 'UNKNOWN_NATIVE_ERROR',
+};
+
+export class SiliconError extends Error {
+  readonly code: SiliconErrorCode;
+  readonly nativeStack?: string;
+
+  constructor(code: SiliconErrorCode, message: string, cause?: unknown) {
+    super(message);
+    this.name = 'SiliconError';
+    this.code = code;
+    this.cause = cause; 
+    
+    // Maintain stack trace (standard JS)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, SiliconError);
+    }
+  }
+}
