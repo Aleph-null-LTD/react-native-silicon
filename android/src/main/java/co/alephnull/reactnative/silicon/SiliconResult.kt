fa1174fd00f6a9202ea1e7d68feb4225140ff3f6
SSHardy <sammy@aleph-null.co.uk>
2026-05-10 03:05:56 +0100

    refactor(android): Add SiliconResult

---
+++ android/src/main/java/co/alephnull/reactnative/silicon/SiliconResult.kt
@@ -0,0 +1,31 @@
+package co.alephnull.reactnative.silicon
+
+/**
+ * A standardized result for the Silicon bridge.
+ * [T] is the type of data returned on success.
+ */
+sealed class SiliconResult<out T> {
+    data class Success<out T>(val data: T) : SiliconResult<T>()
+
+    data class Failure(
+        val code: String,
+        val message: String? = null
+    ) : SiliconResult<Nothing>()
+
+    /**
+     * Converts the Result into a Map that the Expo Bridge can serialize to JS.
+     */
+    fun toBridgeMap(): Map<String, Any?> {
+        return when (this) {
+            is Success -> mapOf(
+                "success" to true,
+                "data" to data
+            )
+            is Failure -> mapOf(
+                "success" to false,
+                "errorCode" to code,
+                "errorMessage" to message
+            )
+        }
+    }
+}
