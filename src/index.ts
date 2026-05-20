0b312a8266131dc51a4a6414f2fb99e961a7de88
SSHardy <sammy@aleph-null.co.uk>
2026-05-20 16:23:04 +0100

    chore: Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 export * from './core/device/index'
 export * from './core/keys/index';
-export * from './core/random-generator/index';
 export * from './core/operations/index';
+export * from './core/random-generator/index';
+export * from './jose/index'
 export * from './errors';
-
-export function GetSiliconStatus() {
-    return 'This lib is a WIP';
-}
