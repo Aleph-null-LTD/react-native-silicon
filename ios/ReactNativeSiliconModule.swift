7a4ae671b4afe7b5d9ab3c046f4efc66c8a641c6
SSHardy <sammy@aleph-null.co.uk>
2026-05-23 11:03:32 +0100

    feat(ios): Implement getCapabilities

--- ios/ReactNativeSiliconModule.swift
+++ ios/ReactNativeSiliconModule.swift
@@ -10,39 +10,92 @@ public class ReactNativeSiliconModule: Module {
     // The module will be accessible from `requireNativeModule('ReactNativeSilicon')` in JavaScript.
     Name("ReactNativeSilicon")
 
-    // Defines constant property on the module.
-    Constant("PI") {
-      Double.pi
+    AsyncFunction("getCapabilities") { () -> [String: Any] in
+      return SiliconDevice.getCapabilities().toBridgeMap()
     }
 
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
+    /*
 
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      return "Hello world! 👋"
+    // ---- Keystore Manager ----
+
+    AsyncFunction("genKey") { (alias: String, opts: GenerateKeyOptions) in
+        result = keystoreManager.genKey(alias, opts)
+        return@AsyncFunction result.toBridgeMap()
     }
 
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { (value: String) in
-      // Send an event to JavaScript.
-      self.sendEvent("onChange", [
-        "value": value
-      ])
+    AsyncFunction("deleteKey") { (alias: String) in
+        val result = keystoreManager.deleteKey(alias)
+        return@AsyncFunction result.toBridgeMap()
     }
 
-    // Enables the module to be used as a native view. Definition components that are accepted as part of the
-    // view definition: Prop, Events.
-    View(ReactNativeSiliconView.self) {
-      // Defines a setter for the `url` prop.
-      Prop("url") { (view: ReactNativeSiliconView, url: URL) in
-        if view.webView.url != url {
-          view.webView.load(URLRequest(url: url))
-        }
-      }
-
-      Events("onLoad")
+    AsyncFunction("deleteAllKeys") { (prefix: String?) in
+        val result = keystoreManager.deleteAllKeys(prefix)
+        return@AsyncFunction result.toBridgeMap()
     }
+
+    AsyncFunction("keyExists") { (alias: String) in
+        val result = keystoreManager.keyExists(alias)
+        return@AsyncFunction result.toBridgeMap()
+    }
+
+    AsyncFunction("listKeys") { (prefix: String?) in
+        val result = keystoreManager.listKeys(prefix)
+        return@AsyncFunction result.toBridgeMap()
+    }
+
+    AsyncFunction("getPubKey") { (alias: String, format: PubkeyFormat) in
+        val result = keystoreManager.getPubKey(alias, format)
+        return@AsyncFunction result.toBridgeMap()
+    }
+
+    AsyncFunction("attestKey") { (alias: String) in
+        val result = keystoreManager.attestKey(alias)
+        return@AsyncFunction result.toBridgeMap()
+    }
+
+    AsyncFunction("getKeyInfo") { (alias: String) in
+        val result = keystoreManager.getKeyInfo(alias)
+        return@AsyncFunction result.toBridgeMap()
+    }
+
+    AsyncFunction("validateKey") { (alias: String) in
+        val result = keystoreManager.validateKey(alias)
+        return@AsyncFunction result.toBridgeMap()
+    }
+
+    // ---- Sign/Verify ----
+
+    AsyncFunction("sign") Coroutine { alias: String, payloadStr: String?, payloadByteArr: ByteArray?, opts: SignOptions ->
+        val bridgePayload = BridgePayloadRecord()
+        bridgePayload.text = payloadStr
+        bridgePayload.bytes = payloadByteArr
+
+        val result = signer.sign(alias, bridgePayload.toPayloadType(), opts)
+        return@Coroutine result.toBridgeMap()
+    }
+
+    AsyncFunction("verify") { payloadStr: String?, payloadByteArr: ByteArray?, signatureB64: String, opts: VerifyOptions ->
+        val bridgePayload = BridgePayloadRecord()
+        bridgePayload.text = payloadStr
+        bridgePayload.bytes = payloadByteArr
+
+        val result = verifier.verify(bridgePayload.toPayloadType(), signatureB64, opts)
+        return@AsyncFunction result.toBridgeMap()
+    }
+
+    // ---- Random Generator ----
+
+    AsyncFunction("generateSecureRandomBytes") { (length: Int, format: RandomBytesFormat) in
+        val result = randomGen.generate(length, format)
+        return@AsyncFunction result.toBridgeMap()
+    }
+
+    // ---- JOSE ----
+
+    AsyncFunction("getJwk") { alias: String ->
+        val result = jose.getJwk(alias)
+        return@AsyncFunction result.toBridgeMap()
+    }
+    */
   }
 }
