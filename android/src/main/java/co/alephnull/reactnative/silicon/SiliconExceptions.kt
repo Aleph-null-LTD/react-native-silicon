273ae19aa3d16f2d3cee8eece6fc76c6ec5d2be9
SSHardy <sammy@aleph-null.co.uk>
2026-06-09 01:47:01 +0100

    chore(android): Remove SiliconException

--- android/src/main/java/co/alephnull/reactnative/silicon/SiliconExceptions.kt
+++
@@ -1,9 +0,0 @@
-package co.alephnull.reactnative.silicon
-
-import expo.modules.kotlin.exception.CodedException
-
-open class SiliconException(
-    code: String,
-    message: String,
-    cause: Throwable? = null
-) : CodedException(code, message, cause)
