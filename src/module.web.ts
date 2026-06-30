43b5d0bb17f03292c83c288f9f1427158bcba8ce
SSHardy <sammy@aleph-null.co.uk>
2026-06-30 04:03:28 +0100

    fix: Correct return to undefined for web module functions

--- src/module.web.ts
+++ src/module.web.ts
@@ -1,67 +1,65 @@
 import { registerWebModule, NativeModule } from 'expo';
-import { AttestResult, GenerateKeyOpts, KeyInfo } from './core/keys/types';
+import { GenerateKeyOpts} from './core/keys/types';
 import { SiliconError, SiliconErrorCode } from './errors';
-import { BridgeResult } from './types/bridge-result';
 import { SignOpts } from './core/operations/types';
 import { BridgeVerifyOpts } from './core/operations/brige-types';
-import { Capabilities } from './core/device';
-import { RandomBytesFormat, RandomGenFormatTypeMap } from './core/random-generator';
+import { RandomBytesFormat } from './core/random-generator';
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 class ReactNativeSiliconModule extends NativeModule {
-  public async getCapabilities(): Promise<BridgeResult<Capabilities>> {
+  public async getCapabilities(): Promise<undefined> {
     throw new SiliconError(SiliconErrorCode.PLATFORM_NOT_SUPPORTED, "This feature is not available for web");
   }
   
-  public async genKey(alias: string, opts: GenerateKeyOpts): Promise<BridgeResult<string>> {
+  public async genKey(alias: string, opts: GenerateKeyOpts): Promise<undefined> {
     throw new SiliconError(SiliconErrorCode.PLATFORM_NOT_SUPPORTED, "This feature is not available for web");
   }
   
-  public async deleteKey(alias: string): Promise<BridgeResult<boolean>> {
+  public async deleteKey(alias: string): Promise<undefined> {
     throw new SiliconError(SiliconErrorCode.PLATFORM_NOT_SUPPORTED, "This feature is not available for web");
   }
   
-  public async deleteAllKeys(prefix?: string): Promise<BridgeResult<number>> {
+  public async deleteAllKeys(prefix?: string): Promise<undefined> {
     throw new SiliconError(SiliconErrorCode.PLATFORM_NOT_SUPPORTED, "This feature is not available for web");
   }
   
-  public async keyExists(alias: string): Promise<BridgeResult<boolean>> {
+  public async keyExists(alias: string): Promise<undefined> {
     throw new SiliconError(SiliconErrorCode.PLATFORM_NOT_SUPPORTED, "This feature is not available for web");
   }
   
-  public async listKeys(prefix?: string): Promise<BridgeResult<string[]>> {
+  public async listKeys(prefix?: string): Promise<undefined> {
     throw new SiliconError(SiliconErrorCode.PLATFORM_NOT_SUPPORTED, "This feature is not available for web");
   }
   
-  public async getPubKey(alias: string, format: 'PEM' | 'B64'): Promise<BridgeResult<string>> {
+  public async getPubKey(alias: string, format: 'PEM' | 'B64'): Promise<undefined> {
     throw new SiliconError(SiliconErrorCode.PLATFORM_NOT_SUPPORTED, "This feature is not available for web");
   }
   
-  public async attestKey(alias: string): Promise<BridgeResult<AttestResult>> {
+  public async attestKey(alias: string): Promise<undefined> {
     throw new SiliconError(SiliconErrorCode.PLATFORM_NOT_SUPPORTED, "This feature is not available for web");
   }
   
-  public async getKeyInfo(alias: string): Promise<BridgeResult<KeyInfo>> {
+  public async getKeyInfo(alias: string): Promise<undefined> {
     throw new SiliconError(SiliconErrorCode.PLATFORM_NOT_SUPPORTED, "This feature is not available for web");
   }
 
-  public async validateKey(alias: string): Promise<BridgeResult<'VALID' | 'MISSING' | 'INVALIDATED' | 'UNRECOVERABLE'>> {
+  public async validateKey(alias: string): Promise<undefined> {
     throw new SiliconError(SiliconErrorCode.PLATFORM_NOT_SUPPORTED, "This feature is not available for web");
   }
 
-  public async sign(alias: string, payload: string | Uint8Array, opts: SignOpts): Promise<BridgeResult<string>> {
+  public async sign(alias: string, payload: string | Uint8Array, opts: SignOpts): Promise<undefined> {
     throw new SiliconError(SiliconErrorCode.PLATFORM_NOT_SUPPORTED, "This feature is not available for web");
   }
 
-  public async verify(payload: string | Uint8Array, signature: string, opts: BridgeVerifyOpts): Promise<BridgeResult<boolean>> {
+  public async verify(payload: string | Uint8Array, signature: string, opts: BridgeVerifyOpts): Promise<undefined> {
     throw new SiliconError(SiliconErrorCode.PLATFORM_NOT_SUPPORTED, "This feature is not available for web");
   }
 
-  public async generateSecureRandomBytes<T extends RandomBytesFormat>(length: number, format: T): Promise<BridgeResult<RandomGenFormatTypeMap[T]>>  {
+  public async generateSecureRandomBytes<T extends RandomBytesFormat>(length: number, format: T): Promise<undefined>  {
     throw new SiliconError(SiliconErrorCode.PLATFORM_NOT_SUPPORTED, "This feature is not available for web");
   }
   
-  public async getJwk(alias: string): Promise<BridgeResult<Record<string, unknown>>>  {
+  public async getJwk(alias: string): Promise<undefined>  {
     throw new SiliconError(SiliconErrorCode.PLATFORM_NOT_SUPPORTED, "This feature is not available for web");
   }
 }
