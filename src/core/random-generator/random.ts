e4a3ee226062ef13aa2257a7662e88ecaa25fa7f
SSHardy <sammy@aleph-null.co.uk>
2026-07-14 04:57:44 +0100

    fix: Prevent type errors in generateSecureRandomBytes()

--- src/core/random-generator/random.ts
+++ src/core/random-generator/random.ts
@@ -9,16 +9,16 @@ import { handleBridgeResult } from "../../utils/handle-bridge-result";
  * Generates random bytes using the secure hardware random generator.
  * @param length The number of bytes 
  * @param format The format of the returned bytes:
- * * B64URL - Base64Url string (default)
+ * * B64URL - Base64Url string
  * * B64 - Standard Base64 string
- * * BYTES - Uint8Array of the bytes
+ * * BYTES - Uint8Array of the bytes (default)
  * @returns The formatted/raw random bytes
  */
 export async function generateSecureRandomBytes<T extends RandomBytesFormat = typeof randomBytesFormats.BYTES>(length: number, format: T = randomBytesFormats.BYTES as T): Promise<RandomGenFormatTypeMap[T]> {
-    if (typeof length !== 'number') throw new SiliconError(SiliconErrorCode.INVALID_ARGUMENT, "[RN-Silicon] 'length' must be of type 'number'");
+    if (typeof length !== 'number' || isNaN(length)) throw new SiliconError(SiliconErrorCode.INVALID_ARGUMENT, "[RN-Silicon] length must be of type number");
     
     if (typeof format !== 'string' || !isRandomBytesFormat(format)) {
-        throw new SiliconError(SiliconErrorCode.INVALID_ARGUMENT, `[RN-Silicon] 'format' invalid, expected ${Object.values(randomBytesFormats).join("|")}`);
+        throw new SiliconError(SiliconErrorCode.INVALID_ARGUMENT, `[RN-Silicon] format invalid, expected ${Object.values(randomBytesFormats).join("|")}`);
     }
 
     length = Math.round(length);
@@ -30,7 +30,8 @@ export async function generateSecureRandomBytes<T extends RandomBytesFormat = ty
     
     if (format === "BYTES") {
         ret = ensureUint8Array(randomBytes) as RandomGenFormatTypeMap[T];
-    } else {
+    } else { // "B64" and "B64URL"
+        if (typeof randomBytes !== 'string') throw new SiliconError(SiliconErrorCode.INTERNAL_ERROR, `[RN-Silicon] bridge returned a non-string result (type: ${typeof randomBytes}) for format ${format}`);
         ret = randomBytes;
     }
 
