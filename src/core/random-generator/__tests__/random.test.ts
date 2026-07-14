70e76ea49be25ee048d5743a1d45ce5e9a8f7015
SSHardy <sammy@aleph-null.co.uk>
2026-07-14 04:56:54 +0100

    test: Add tests for random.ts

---
+++ src/core/random-generator/__tests__/random.test.ts
@@ -0,0 +1,182 @@
+import { vi, describe, it, expect } from "vitest";
+import ReactNativeSiliconModule from '../../../module';
+import { createBridgeFailure, createBridgeSuccess } from "../../../__test_utils__/factories/bridge-result";
+import { generateSecureRandomBytes } from "../random";
+import { SiliconError, SiliconErrorCode } from "../../../errors";
+import fc from 'fast-check';
+import { test } from '@fast-check/vitest';
+
+describe('generateSecureRandomBytes()', () => {
+    
+    const mockStringResult = () => {
+        const defaultMockData = 'some-random-string';
+        const defaultMockVal = createBridgeSuccess(defaultMockData);
+        vi.mocked(ReactNativeSiliconModule.generateSecureRandomBytes).mockResolvedValue(defaultMockVal);
+        
+        return defaultMockData;
+    };
+
+    const mockUint8ArrayResult = () => {
+        const defaultMockData = new Uint8Array([10, 20, 30]);
+        const defaultMockVal = createBridgeSuccess(defaultMockData);
+        vi.mocked(ReactNativeSiliconModule.generateSecureRandomBytes).mockResolvedValue(defaultMockVal);
+
+        return defaultMockData;
+    };
+
+    it("should successfully return a Uint8Array when format is 'BYTES'", async () => {
+        const defaultMockData = mockUint8ArrayResult();
+
+        await expect(generateSecureRandomBytes(3, 'BYTES')).resolves.toStrictEqual(defaultMockData);
+        expect(ReactNativeSiliconModule.generateSecureRandomBytes).toHaveBeenCalledOnce();
+    });
+
+    it("should throw when bridge returns a failure result", async () => {
+        const defaultMockVal = createBridgeFailure(SiliconErrorCode.INTERNAL_ERROR, 'some random error message', 'some-random-error-stack');
+        vi.mocked(ReactNativeSiliconModule.generateSecureRandomBytes).mockResolvedValueOnce(defaultMockVal);
+
+        let didThrow = true;
+        try {
+            await generateSecureRandomBytes(3, 'BYTES');
+            didThrow = false;
+        } catch (error) {
+            expect(error).toBeInstanceOf(SiliconError);
+        }
+
+        if (!didThrow) expect.fail('expected generateSecureRandomBytes() to throw, but it did not');
+        
+        expect(ReactNativeSiliconModule.generateSecureRandomBytes).toHaveBeenCalledOnce();
+    });
+
+    it("should throw when bridge result contains a string but format is 'BYTES'", async () => {
+        const defaultMockData = mockStringResult();
+
+        let didThrow = true;
+        try {
+            await generateSecureRandomBytes(3, 'BYTES');
+            didThrow = false;
+        } catch (error) {
+            expect(error).toBeInstanceOf(SiliconError);
+        }
+
+        if (!didThrow) expect.fail('expected generateSecureRandomBytes() to throw, but it did not');
+        
+        expect(ReactNativeSiliconModule.generateSecureRandomBytes).toHaveBeenCalledOnce();
+    });
+
+    it("should successfully return a string when format is 'B64'", async () => {
+        const defaultMockData = mockStringResult();
+
+        await expect(generateSecureRandomBytes(3, 'B64')).resolves.toBe(defaultMockData);
+        expect(ReactNativeSiliconModule.generateSecureRandomBytes).toHaveBeenCalledOnce();
+    });
+
+    it("should throw when bridge result contains a Uint8Array but format is 'B64'", async () => {
+        const defaultMockData = mockUint8ArrayResult();
+
+        let didThrow = true;
+        try {
+            await generateSecureRandomBytes(3, 'B64');
+            didThrow = false;
+        } catch (error) {
+            expect(error).toBeInstanceOf(SiliconError);
+        }
+
+        if (!didThrow) expect.fail('expected generateSecureRandomBytes() to throw, but it did not');
+        
+        expect(ReactNativeSiliconModule.generateSecureRandomBytes).toHaveBeenCalledOnce();
+    });
+
+    it("should successfully return a string when format is 'B64URL'", async () => {
+        const defaultMockData = mockStringResult();
+
+        await expect(generateSecureRandomBytes(3, 'B64URL')).resolves.toBe(defaultMockData);
+        expect(ReactNativeSiliconModule.generateSecureRandomBytes).toHaveBeenCalledOnce();
+    });
+
+    it("should throw when bridge result contains a Uint8Array but format is 'B64URL'", async () => {
+        const defaultMockData = mockUint8ArrayResult();
+
+        let didThrow = true;
+        try {
+            await generateSecureRandomBytes(3, 'B64URL');
+            didThrow = false;
+        } catch (error) {
+            expect(error).toBeInstanceOf(SiliconError);
+        }
+
+        if (!didThrow) expect.fail('expected generateSecureRandomBytes() to throw, but it did not');
+        
+        expect(ReactNativeSiliconModule.generateSecureRandomBytes).toHaveBeenCalledOnce();
+    });
+
+    test.prop(
+        [fc.anything()]
+    )('should throw when length is not of type number', async (chaoticData) => {
+        const defaultMockData = mockUint8ArrayResult();
+        
+        if (typeof chaoticData == 'number') {
+            await expect(generateSecureRandomBytes(chaoticData, 'BYTES')).resolves.toStrictEqual(defaultMockData);
+            expect(ReactNativeSiliconModule.generateSecureRandomBytes).toHaveBeenCalledOnce();
+
+        } else {
+            let didThrow = true;
+            try {
+                // @ts-expect-error
+                await generateSecureRandomBytes(chaoticData, 'BYTES')
+                didThrow = false;
+            } catch (error) {
+                expect(error).toBeInstanceOf(SiliconError);
+            }
+
+            if (!didThrow) expect.fail('expected generateSecureRandomBytes() to throw, but it did not');
+
+            expect(ReactNativeSiliconModule.generateSecureRandomBytes).toHaveBeenCalledTimes(0);
+        }
+
+        vi.clearAllMocks();
+    });
+
+    test.prop(
+        [
+            fc.oneof(
+                fc.constant('BYTES'),
+                fc.constant('B64'),
+                fc.constant('B64URL'),
+                fc.constant(undefined),
+                fc.anything()
+            )
+        ]
+    )('should throw when format is not a valid string literal and is not undefined', async (chaoticData) => {
+        if (chaoticData === 'BYTES' ||
+            chaoticData === undefined
+        ) {
+            const defaultMockData = mockUint8ArrayResult();
+            await expect(generateSecureRandomBytes(3, chaoticData)).resolves.toStrictEqual(defaultMockData);
+            expect(ReactNativeSiliconModule.generateSecureRandomBytes).toHaveBeenCalledOnce();
+
+        } else if (chaoticData === 'B64' ||
+            chaoticData === 'B64URL'
+        ) {
+            const defaultMockData = mockStringResult();
+            await expect(generateSecureRandomBytes(3, chaoticData)).resolves.toBe(defaultMockData);
+            expect(ReactNativeSiliconModule.generateSecureRandomBytes).toHaveBeenCalledOnce();
+
+        } else {
+            let didThrow = true;
+            try {
+                // @ts-expect-error
+                await generateSecureRandomBytes(3, chaoticData)
+                didThrow = false;
+            } catch (error) {
+                expect(error).toBeInstanceOf(SiliconError);
+            }
+
+            if (!didThrow) expect.fail('expected generateSecureRandomBytes() to throw, but it did not');
+
+            expect(ReactNativeSiliconModule.generateSecureRandomBytes).toHaveBeenCalledTimes(0);
+        }
+
+        vi.clearAllMocks();
+    });
+});
