72902a7da2da953be7a8e916cc5a91a5fd7c4603
SSHardy <sammy@aleph-null.co.uk>
2026-07-21 00:12:27 +0100

    fix: Prevent empty alias string and invalid digests in getJwk()

--- src/jose/jwk.ts
+++ src/jose/jwk.ts
@@ -1,4 +1,5 @@
 import { SignDigest } from '../core/operations';
+import { isSignDigest, signDigest } from '../core/operations/constants';
 import { SiliconError, SiliconErrorCode } from '../errors';
 import NativeSilicon from '../module';
 import { handleBridgeResult } from '../utils/handle-bridge-result';
@@ -10,8 +11,9 @@ import { Jwk } from './types';
  * @returns 
  */
 export async function getJwk(alias: string, digest?: SignDigest): Promise<Jwk> {
-    if (typeof alias !== 'string') throw new SiliconError(SiliconErrorCode.INVALID_ARGUMENT, "[RN-Silicon] alias must be of type string");
-    
+    if (typeof alias != 'string' || alias.trim().length < 1) throw new SiliconError(SiliconErrorCode.INVALID_ARGUMENT, "[RN-Silicon] alias must be of type string");
+    if (digest !== undefined && !isSignDigest(digest)) throw new SiliconError(SiliconErrorCode.INVALID_ARGUMENT, `[RN-Silicon] digest must be of type ${Object.values(signDigest).join(' | ')}`);
+
     const result = await NativeSilicon.getJwk(alias, digest);
     return handleBridgeResult(result);
 }
