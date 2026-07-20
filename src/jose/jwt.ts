15542a89cbe5765a3992ffdb5e815a1c604fa8f3
SSHardy <sammy@aleph-null.co.uk>
2026-07-20 18:16:05 +0100

    fix: Prevent empty alias string in signJwt()

--- src/jose/jwt.ts
+++ src/jose/jwt.ts
@@ -16,15 +16,15 @@ import { JwtHeader, JwtPayload } from "./types";
  * @returns The signed JWT
  */
 export async function signJwt(alias: string, header: JwtHeader, payload: JwtPayload, digest?: SignDigest): Promise<string> {
-    if (typeof alias !== 'string') throw new SiliconError(SiliconErrorCode.INVALID_ARGUMENT, "[RN-Silicon] 'alias' must be of type 'string'");
-    if (!isPlainObject(header)) throw new SiliconError(SiliconErrorCode.INVALID_ARGUMENT, "[RN-Silicon] 'header' must be an object");
-    if (!isPlainObject(payload)) throw new SiliconError(SiliconErrorCode.INVALID_ARGUMENT, "[RN-Silicon] 'payload' must be an object");
+    if (typeof alias !== 'string' || alias.trim().length < 1) throw new SiliconError(SiliconErrorCode.INVALID_ARGUMENT, "[RN-Silicon] alias must be of type string");
+    if (!isPlainObject(header)) throw new SiliconError(SiliconErrorCode.INVALID_ARGUMENT, "[RN-Silicon] header must be an object");
+    if (!isPlainObject(payload)) throw new SiliconError(SiliconErrorCode.INVALID_ARGUMENT, "[RN-Silicon] payload must be an object");
   
     if (digest !== undefined &&
       (typeof digest !== 'string' || !isSignDigest(digest))
     ) {
       console.log("in here")
-      throw new SiliconError(SiliconErrorCode.INVALID_ARGUMENT, `[RN-Silicon] 'digest' must be of type ${Object.values(signDigest).join("|")}`);
+      throw new SiliconError(SiliconErrorCode.INVALID_ARGUMENT, `[RN-Silicon] digest must be of type ${Object.values(signDigest).join("|")}`);
     }
   
     // Encode the Header and Payload, then concat them with a '.' seperator
