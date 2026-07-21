b5d03d88bfa3e1088f5bc41aefaa60cb09e32f6a
SSHardy <sammy@aleph-null.co.uk>
2026-07-21 04:02:07 +0100

    test: Add tests for encoding.ts

---
+++ src/utils/__tests__/encoding.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect } from "vitest";
+import { test } from "@fast-check/vitest";
+import fc from 'fast-check';
+import { objectToBase64Url } from "../encoding";
+
+describe('objectToBase64Url()', () => {
+    const Base64UrlRegex = /^(?:[A-Za-z0-9\-_]{4})*(?:[A-Za-z0-9\-_]{2,3})?$/;
+
+    test.prop(
+        [fc.object()]
+    )('Should return valid Base64Url when obj is an object', (validObj) => {
+        expect(objectToBase64Url(validObj)).matches(Base64UrlRegex);
+    });
+});
