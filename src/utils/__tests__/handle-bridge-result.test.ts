296c87a09853450f9c840b55a11d75dc640d7b24
SSHardy <sammy@aleph-null.co.uk>
2026-07-21 04:12:35 +0100

    test: Add tests for handle-bridge-result.ts

---
+++ src/utils/__tests__/handle-bridge-result.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it } from "vitest";
+import { handleBridgeResult } from "../handle-bridge-result";
+import { createBridgeSuccess, createBridgeFailure } from "../../__test_utils__/factories/bridge-result";
+import { test } from "@fast-check/vitest";
+import fc from "fast-check";
+import { SiliconError, SiliconErrorCode } from "../../errors";
+
+describe('handleBridgeResult()', () => {
+    test.prop(
+        [fc.anything()]
+    )('should return the extracted data from BridgeResult when it is a success', (chaoticData) => {
+        expect(
+            handleBridgeResult(createBridgeSuccess(chaoticData))
+        ).toBe(chaoticData)
+    });
+    
+    it('should throw when BridgeResult is a failure', () => {
+        expect(
+            () => handleBridgeResult(createBridgeFailure(SiliconErrorCode.INTERNAL_ERROR, "some random error message", "some-random-native-stack"))
+        ).toThrow(SiliconError);
+    });
+});
