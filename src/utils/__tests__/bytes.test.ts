b355e7642cd24e41c4ccc8f590b57c807b18935f
SSHardy <sammy@aleph-null.co.uk>
2026-07-21 03:55:09 +0100

    test: Add tests for bytes.ts

---
+++ src/utils/__tests__/bytes.test.ts
@@ -0,0 +1,47 @@
+import { describe, it, expect } from "vitest";
+import { test } from "@fast-check/vitest";
+import fc from "fast-check";
+import { ensureUint8Array } from "../bytes";
+import { SiliconError } from "../../errors";
+
+describe('ensureUint8Array()', () => {
+    test.prop(
+        [fc.uint8Array()]
+    )('should successfully return the exact data when data is Uint8Array', (validData) => {
+        const result = ensureUint8Array(validData);
+        
+        expect(result).toBeInstanceOf(Uint8Array);
+        expect(result).toEqual(validData);
+    });
+    
+    test.prop(
+        [fc.uint8Array()]
+    )('should successfully convert arrays of valid bytes', (validData) => {
+        const arr = Array.from(validData);
+        const result = ensureUint8Array(arr);
+        
+        expect(result).toBeInstanceOf(Uint8Array);
+        expect(result).toEqual(validData);
+    });
+
+    test.prop(
+        [
+            fc.array(
+                fc.anything().filter(data => typeof data !== 'number'), 
+                { 
+                    minLength: 1 
+                }
+            )
+        ]
+    )('should throw when an array contains invalid data types', (invalidArray) => {
+        expect(() => ensureUint8Array(invalidArray)).toThrow(SiliconError);
+    });
+    
+    test.prop(
+        [
+            fc.anything().filter((data) => !(data instanceof Uint8Array || Array.isArray(data) || data instanceof ArrayBuffer))
+        ]
+    )('should throw when data is not a Uint8Array, Array, or ArrayBuffer', (chaoticData) => {
+        expect(() => ensureUint8Array(chaoticData)).toThrow(SiliconError);
+    });
+});
